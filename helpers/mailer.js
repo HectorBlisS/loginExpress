775ee426518f0e057e0f0c77c94aa8b2a3bda5f8
hectorbliss <contacto@fixter.org>
2018-07-12 12:08:27 -0500

    template

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -1,4 +1,6 @@
 const nodemailer = require('nodemailer');
+const hbs = require('hbs');
+const fs = require('fs');
 
 const transporter = nodemailer.createTransport({
     service: 'Gmail',
@@ -20,3 +22,18 @@ exports.sendWelcomeMail = (user)=>{
       .then(info => console.log(info))
       .catch(error => console.log(error))
 }
+
+const welcomeCompile = hbs.compile(fs.readFileSync((__dirname, './views/welcome.hbs'), 'utf8'));
+
+exports.sendTemplate = (user) => {
+    const data = {
+        from: 'Kiubo? 👻 <fixtergeek@gmail.com>',
+        to: user.email, 
+        subject: 'Probando esta', 
+        //text: `Hola ${user.username} Bienvenido a nuestra ironApp`,
+        html: welcomeCompile(user)
+      }
+    transporter.sendMail(data)
+      .then(info => console.log(info))
+      .catch(error => console.log(error))
+} ;
